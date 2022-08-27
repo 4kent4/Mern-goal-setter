41dddb2891506c11bd189ef3ed45f60467cbb3fd
4kent4 <kentfortich.ccs@gmail.com>
2022-08-27 20:20:14 +0800

    added routes and controller

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,15 +1,24 @@
-const getGoals = (req, res) => {
-    res.json({ message: 'Get Goals'})
-}
-const setGoal = (req, res) => {
-    res.json({ message: 'Set Goals'})
-}
-const updateGoal = (req, res) => {
-    res.json({ message: `Update Goals ${req.params.id}`})
-}
-const deleteGoal = (req, res) => {
-    res.json({ message: `Delete Goals ${req.params.id}`})
-}
+const asyncHandler = require('express-async-handler')
+
+const getGoals = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Get Goals'})
+})
+
+const setGoal = asyncHandler(async (req, res) => {
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+
+    res.status(200).json({ message: 'Set Goals'})
+})
+
+const updateGoal = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Update Goals ${req.params.id}`})
+})
+const deleteGoal = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Delete Goals ${req.params.id}`})
+})
 
 module.exports = {
     getGoals, setGoal, updateGoal, deleteGoal
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,12 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
-router.get('/', getGoals)
-
-router.post('/', setGoal )
-
-router.put('/:id', updateGoal)
-
-router.delete('/:id', deleteGoal)
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').put(updateGoal).delete(deleteGoal)
 
 module.exports = router
